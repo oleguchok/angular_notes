dd2e4ba015decab3b36c2d5e15fdc4d46ab9876f
oleguchok <oleguchok@gmail.com>
2015-07-10 22:59:15 +0300

    Add adding notes form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,35 @@
 
 
 angular.module('angularNotesApp')
-  .controller('MainCtrl', function (localStorageService) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+
+    var notesInStore = localStorageService.get('notes');
+
+    $scope.notes = notesInStore || [];
+    $scope.visibleAddForm = true;
+
+    $scope.$watch('notes', function() {
+      localStorageService.set('notes', $scope.notes);
+    }, true);
+
+    $scope.addNote = function() {
+      var note = {
+        title : $scope.note.title,
+        body : $scope.note.body
+      };
+      $scope.notes.push(note);
+      $scope.note.title = "";
+      $scope.note.body = "";
+    };
+
+    $scope.toggle = function() {
+      $scope.visibleAddForm = !$scope.visibleAddForm;
+      $scope.note.title = "";
+      $scope.note.body = "";
+    };
+
+    $scope.clear = function() {
+      $scope.note.body = "";
+    };
+
   });
