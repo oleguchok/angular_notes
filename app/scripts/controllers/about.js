8bb401270e6415a2cb8df43a0979b892c4177538
oleguchok <oleguchok@gmail.com>
2015-07-10 22:23:20 +0300

    first commit with yeoman scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularNotesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularNotesApp
+ */
+angular.module('angularNotesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularNotesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularNotesApp
+ */
+angular.module('angularNotesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
